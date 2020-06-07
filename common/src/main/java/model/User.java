9d9675c86c4a25d19d089379020674f768b22c7e
zhy <106460810@qq.com>
2020-06-07 10:23:14 +0800

    自己练习项目

---
+++ common/src/main/java/model/User.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.io.Serializable;
+
+/**
+ * 用户基础类
+ */
+public class User implements Serializable {
+    // 用户名称
+    private String userName;
+    // 登录账号
+    private String loginName;
+    // 密码
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
